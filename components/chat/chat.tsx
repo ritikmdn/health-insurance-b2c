0513144d867730cc179411b1c4c2b9372ccc78b1
Ritik Madan <ritikmdn@gmail.com>
2023-12-04 05:35:15 +0530

    Updates empty page on /chat

--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -46,7 +46,7 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
 
   return (
     <>
-      <div className={cn('pb-[200px] pt-4 md:pt-10', className)}>
+      <div className={cn('z-10 pb-[200px] pt-4 md:pt-10', className)}>
         {messages.length ? (
           <>
             <ChatList messages={messages} />
--- components/chat/empty-screen.tsx
+++ components/chat/empty-screen.tsx
@@ -13,8 +13,8 @@ const exampleMessages = [
     message: 'Summarize the following article for a 2nd grader: \n'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Tax benefits of health insurance',
+    message: `What is the maximum deduction under Section 80D for health insurance plans?`
   }
 ]
 
@@ -23,13 +23,13 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Hey, I'm Simple AI Insurance Advisor 👋
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}Next.js and{' '}Vercel KV.
+          I'm here to answer your queries related to health insurance.
         </p>
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          You can start a conversation directly or try the following examples:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
@@ -44,6 +44,9 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
             </Button>
           ))}
         </div>
+        <p className="mt-4 leading-normal text-gray italic">
+          Note: The AI advisor is experimental and can make mistakes. We recommend talking to a professional advisor before purchasing an insurance policy.   
+        </p>
       </div>
     </div>
   )
